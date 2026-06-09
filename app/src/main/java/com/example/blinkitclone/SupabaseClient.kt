278ca7156bd4ca1c71517d573bc413a504e50128
Rishicharhate <rishipatilcharhate22@gmail.com>
2026-06-09 20:01:09 +0530

    Update SupabaseClient with placeholder values

--- app/src/main/java/com/example/blinkitclone/SupabaseClient.kt
+++ app/src/main/java/com/example/blinkitclone/SupabaseClient.kt
@@ -7,8 +7,8 @@ import io.github.jan.supabase.postgrest.Postgrest
 object SupabaseClient {
 
     val client = createSupabaseClient(
-        supabaseUrl = "https://wpsqmolucmkbxlnppris.supabase.co",
-        supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Indwc3Ftb2x1Y21rYnhsbnBwcmlzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODAyMTUxNzMsImV4cCI6MjA5NTc5MTE3M30.GgiSKSpMBOJBQ8h8P9wUoiWeWjAtJbKsQWrQI86cncw"
+        supabaseUrl = "Add your supabse project url",
+        supabaseKey = "Add your supabase project key"
     ) {
         install(Auth)
         install(Postgrest)
